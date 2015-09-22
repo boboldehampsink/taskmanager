1bf88f195cc6d3a6c9e0f3028fe63e4e70de4398
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2015-09-22 09:35:11 +0200

    Added the ability to restart a task

--- resources/js/taskManager.js
+++ resources/js/taskManager.js
@@ -9,6 +9,24 @@
   'use strict';
 
   /**
+   * Restart button
+   */
+  $('#content').on('click', 'a.restart', function(e) {
+    e.preventDefault();
+
+    // Make user confirm
+    if (confirm(Craft.t('Are you sure you want to restart this task?'))) {
+
+      // Delete via tasks controller
+      Craft.postActionRequest('tasks/rerunTask', {
+        taskId: $(this).closest('tr').data('id'),
+      }, $.proxy(function() {
+        Craft.cp.displayNotice(Craft.t('Task restarted.'));
+      }, this));
+    }
+  });
+
+  /**
    * Delete button
    */
   $('#content').on('click', 'a.delete', function(e) {
--- translations/nl.php
+++ translations/nl.php
@@ -17,6 +17,10 @@ return array(
     'Created'       => 'Aangemaakt',
     'Current step'  => 'Huidige stap',
     'Total steps'   => 'Totaal aantal stappen',
+    'Restart'       => 'Herstart',
+    'Are you sure you want to restart this task?' => 'Weet je zeker dat je deze taak wilt herstarten?',
+    'Task restarted.' => 'Taak herstart.',
+    'Delete'        => 'Verwijder',
     'Are you sure you want to delete this task?' => 'Weet je zeker dat je deze taak wilt verwijderen?',
     'Task deleted.' => 'Taak verwijderd.',
 );
