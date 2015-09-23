295b32a996f142499671f681db6172d77f09aa2d
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2015-09-23 17:09:35 +0200

    Added endpoint for rerunning all failed tasks

---
+++ controllers/TaskManagerController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Craft;
+
+/**
+ * Task Manager.
+ *
+ * @author    Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
+ * @copyright Copyright (c) 2015, Bob Olde Hampsink
+ * @license   MIT
+ *
+ * @link      http://github.com/boboldehampsink
+ */
+class TaskManagerController extends BaseController
+{
+    /**
+     * Allow anonymous access (for a cronjob perhaps).
+     *
+     * @return [type] [description]
+     */
+    public $allowAnonymous = true;
+
+    /**
+     * Rerun all failed tasks.
+     */
+    public function actionRerunAllFailedTasks()
+    {
+        // Get all failed tasks
+        $tasks = craft()->db->createCommand()
+            ->select('id')
+            ->from('tasks')
+            ->where(array('and', 'lft = 1', 'status = :status'), array(':status' => TaskStatus::Error))
+            ->queryAll();
+
+        // Loop through failed tasks
+        foreach ($tasks as $task) {
+
+            // Rerun task
+            craft()->tasks->rerunTaskById($task['id']);
+        }
+
+        // Run pending tasks controller
+        craft()->runController('tasks/runPendingTasks');
+    }
+}
