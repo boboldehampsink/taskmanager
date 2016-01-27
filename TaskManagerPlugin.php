0a3bcfd6743e4f0ae4a2a8d3d425ae4a57231842
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2016-01-27 08:59:33 +0100

    Added plugin description and documentation url

--- TaskManagerPlugin.php
+++ TaskManagerPlugin.php
@@ -24,6 +24,16 @@ class TaskManagerPlugin extends BasePlugin
     }
 
     /**
+     * Get plugin description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return Craft::t('Adds a "Task Manager" section to your CP to easily cancel or delete Craft Tasks.');
+    }
+
+    /**
      * Get plugin version.
      *
      * @return string
@@ -50,7 +60,17 @@ class TaskManagerPlugin extends BasePlugin
      */
     public function getDeveloperUrl()
     {
-        return 'http://github.com/boboldehampsink';
+        return 'https://github.com/boboldehampsink';
+    }
+
+    /**
+     * Get plugin documentation url.
+     *
+     * @return string
+     */
+    public function getDocumentationUrl()
+    {
+        return 'https://github.com/boboldehampsink/taskmanager';
     }
 
     /**
